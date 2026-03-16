465896007a79ae0dd65c09e04172dd31228fc9be
Anaid-Ariwany <anaidariwany@gmail.com>
2026-03-16 23:55:36 +0300

    DOM manipulation: created the cards and products dynamically.

--- pages/app.js
+++ pages/app.js
@@ -1,56 +1,56 @@
 const products = [
 
     {
-        image: "assets/images/laptop.webp",
+        image: "../assets/images/laptop.webp",
         productName: "MacBook Pro",
         category: "Laptops",
         price: "$1999"
     },
 
     {
-        image: "assets/images/ipad.webp",
+        image: "../assets/images/ipad.webp",
         productName: "iPad Air",
         category: "Tablets",
         price: "$599"
     },
 
     {
-        image: "assets/images/blackHeadset.webp",
+        image: "../assets/images/blackHeadset.webp",
         productName: "Sony WH-1000XM5",
         category: "Headphones",
         price: "$399"
     },
 
     {
-        image: "assets/images/smartWatch.webp",
+        image: "../assets/images/smartWatch.webp",
         productName: "Apple Watch Series 9",
         category: "Wearables",
         price: "$399"
     },
 
     {
-        image: "assets/images/keyboard.webp",
+        image: "../assets/images/keyboard.webp",
         productName: "Mechanical Keyboard",
         category: "Accessories",
         price: "$129"
     },
 
     {
-        image: "assets/images/samsungMonitor.webp",
+        image: "../assets/images/samsungMonitor.webp",
         productName: "Samsung Galaxy",
         category: "Monitors",
         price: "$949"
     },
 
     {
-        image: "assets/images/mouse.webp",
+        image: "../assets/images/mouse.webp",
         productName: "Logitech MX Master",
         category: "Accessories",
         price: "$99"
     },
 
     {
-        image: "assets/images/hdMonitor.webp",
+        image: "../assets/images/hdMonitor.webp",
         productName: "4K Ultra HD Monitor",
         category: "Monitors",
         price: "$799"
@@ -58,7 +58,7 @@ const products = [
 ]
 
 /* select the container */
-const productContainer = document.querySelector('.cardsDiv');
+const productContainer = document.querySelector('.cardDiv');
 
 /* create product cards */
 function createProductCard(product) {
@@ -80,8 +80,8 @@ function createProductCard(product) {
     return card;
 }
 
-/* loop through the first 3 products and add them to the page */
-products.slice(0, 3).forEach((product) => {
+/* add cards to the page */
+products.forEach((product) => {
     const card = createProductCard(product);
     productContainer.appendChild(card);
 });

fa707d5f89823898ca12bd615286f97504b83c65
Anaid-Ariwany <anaidariwany@gmail.com>
2026-03-16 23:03:47 +0300

    Styling: updated button styling for the product cards on both pages.
    Local storage: Created the objects containing the data in the index.js

---
+++ index.js
@@ -0,0 +1,58 @@
+const products = [
+
+    {
+        image: "assets/images/laptop.webp",
+        productName: "MacBook Pro",
+        category: "Laptops",
+        price: "$1999"
+    },
+
+    {
+        image: "assets/images/ipad.webp",
+        productName: "iPad Air",
+        category: "Tablets",
+        price: "$599"
+    },
+
+    {
+        image: "assets/images/blackHeadset.webp",
+        productName: "Sony WH-1000XM5",
+        category: "Headphones",
+        price: "$399"
+    },
+
+    {
+        image: "assets/images/smartWatch.webp",
+        productName: "Apple Watch Series 9",
+        category: "Wearables",
+        price: "$399"
+    },
+
+    {
+        image: "assets/images/keyboard.webp",
+        productName: "Mechanical Keyboard",
+        category: "Accessories",
+        price: "$129"
+    },
+
+    {
+        image: "assets/images/samsungMonitor.webp",
+        productName: "Samsung Galaxy",
+        category: "Monitors",
+        price: "$949"
+    },
+
+    {
+        image: "assets/images/mouse.webp",
+        productName: "Logitech MX Master",
+        category: "Accessories",
+        price: "$99"
+    },
+
+    {
+        image: "assets/images/hdMonitor.webp",
+        productName: "4K Ultra HD Monitor",
+        category: "Monitors",
+        price: "$799"
+    }
+]
